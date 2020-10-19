53e0b3b4fdc5e2db14a7f6e54c1a4256cac2c1d6
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 09:07:23 -0600

    Add deploy repos to project

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-inferv "LATEST"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_username
+                                     :password :env/clojars_password
+                                     :sign-releases false}]]
   :eval-in-leiningen true)
