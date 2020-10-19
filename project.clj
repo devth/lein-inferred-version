3068f85c0436ba65b630284c9399caf9ec5e1c4f
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 09:17:29 -0600

    Use specific versoin

--- project.clj
+++ project.clj
@@ -3,9 +3,15 @@
   :url "https://github.com/devth/lein-inferv"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-inferv "LATEST"]]
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_username
-                                     :password :env/clojars_password
-                                     :sign-releases false}]]
+  :plugins [[lein-inferv "20201019.024318.09c7a3a"]]
+
+  ;; :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+  ;;                                     :username :env/clojars_username
+  ;;                                     :password :env/clojars_password
+  ;;                                     :sign-releases false}]
+  ;;                       ["releases"  {:url "https://clojars.org/repo"
+  ;;                                     :username :env/clojars_username
+  ;;                                     :password :env/clojars_password
+  ;;                                     :sign-releases false}]]
+
   :eval-in-leiningen true)
