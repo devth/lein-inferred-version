ba6be054c6586bd836923e6d764b70ba5a3e809b
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 08:54:56 -0600

    Use lein release

--- deploy-profiles.clj
+++ deploy-profiles.clj
@@ -1,4 +1,4 @@
-{:user
+{:default
  {:deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_username
                                      :password :env/clojars_password
