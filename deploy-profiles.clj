0f1b5972603593ee3a619878e865934e8d04eb94
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 08:32:48 -0600

    Add deploy profile config

---
+++ deploy-profiles.clj
@@ -0,0 +1,5 @@
+{{:user
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_username
+                                     :password :env/clojars_password
+                                     :sign-releases false}]]}}
