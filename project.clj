5d9f1cc3c5ff218b4869977b10144f0e2f478d44
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 09:39:09 -0600

    Fix

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/devth/lein-inferv"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-inferv "20201019.024318.09c7a3a"]]
+  :plugins [[lein-inferv "20201019.024318.09c7a3a"]
+            [lein-pprint "1.3.2"]]
 
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
