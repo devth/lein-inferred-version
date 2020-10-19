c1d1668cb7055aa2a9f76576594300b05dbb8dd9
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 09:37:27 -0600

    Debug

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-inferv "20201019.024318.09c7a3a"]]
 
-  ;; :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
-  ;;                                     :username :env/clojars_username
-  ;;                                     :password :env/clojars_password
-  ;;                                     :sign-releases false}]
-  ;;                       ["releases"  {:url "https://clojars.org/repo"
-  ;;                                     :username :env/clojars_username
-  ;;                                     :password :env/clojars_password
-  ;;                                     :sign-releases false}]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
 
   :eval-in-leiningen true)
