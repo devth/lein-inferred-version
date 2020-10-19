59b22f566d47c18e22058ca46ffb911008708901
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 09:03:27 -0600

    Clean

--- deploy-profiles.clj
+++
@@ -1,5 +0,0 @@
-{:default
- {:deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_username
-                                     :password :env/clojars_password
-                                     :sign-releases false}]]}}
