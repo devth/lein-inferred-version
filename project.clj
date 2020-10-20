64afdf215396586d9b97a047cf67416569ebec2a
Trevor Hartman <trevorhartman@gmail.com>
2020-10-19 19:37:00 -0600

    Compute instant once

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/devth/lein-inferv"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-inferv "20201019.024318.09c7a3a"]
+  :plugins [[lein-inferv "20201019.151901.3068f85"]
             [lein-pprint "1.3.2"]]
 
   :deploy-repositories [["releases"  {:url "https://clojars.org/repo"
